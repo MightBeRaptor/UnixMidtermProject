a0f4f65394f665c9fd8f03e7ec9a29616ab3bd03
Surxe <119145352+Surxe@users.noreply.github.com>
2025-03-11 19:19:31 -0400

    get_metrics.sh initial with testing data output

---
+++ src/get_metrics.sh
@@ -0,0 +1,76 @@
+# Get dependencies
+sudo apt update
+sudo apt install sysstat vmstat iostat dstat -y
+sudo apt install sysstat -y
+sudo apt install ifstat -y
+
+# Store the time in a variable
+filename="metrics_$(date +'%Y-%m-%d_%H-%M-%S').txt"
+
+# Get metrics
+touch "$filename"
+
+# CPU
+top -bn1 | grep "Cpu(s)" >> "$filename"
+# Expected data
+# %Cpu(s):  0.0 us,  0.0 sy,  0.0 ni,100.0 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+# us = user
+# sy = system
+# ni = lower priority processes
+# id = idle
+# wa = waiting for I/O
+# hi = hardware interrupts
+# si = software interrupts
+# st = steal time
+
+# MEMORY
+free -m | grep -E "total|Mem:" >> "$filename"
+# Expected data:
+#                total        used        free      shared  buff/cache   available
+#Mem:             969         492         283           0         344         477
+#Swap:              0           0           0
+
+# DISK
+df -h | grep "sda 1 " >> "$filename"
+# Expected data:
+# /dev/sda1        9.7G 2.7G   6.5G   30% /
+
+# DISK I/O
+iostat -dx 1 1 | grep -E "Device|sda" >> "$filename"
+# Expected data:
+# Device            r/s     rkB/s   rrqm/s  %rrqm r_await rareq-sz     w/s     wkB/s   wrqm/s  %wrqm w_await wareq-sz     d/s     dkB/s   drqm/s  %drqm d_await dareq-sz     f/s f_await  aqu-sz  %util
+# sda              2.32     57.09     0.15   5.99    0.96    24.61    2.25    160.07     0.62  21.74   26.21    71.25    0.03    239.88     0.00   0.00    0.39  7834.06    0.19    0.05    0.06   0.30
+
+# r/s = read requests per second
+# rkB/s = read kilobytes per second
+# rrqm/s = read requests merged per second
+# %rrqm = percentage of read requests merged
+# r_await = read request average wait time
+# rareq-sz = read request average size
+# w/s = write requests per second
+# wkB/s = write kilobytes per second
+# wrqm/s = write requests merged per second
+# %wrqm = percentage of write requests merged
+# w_await = write request average wait time
+# wareq-sz = write request average size
+# d/s = discard requests per second
+# dkB/s = discard kilobytes per second
+# drqm/s = discard requests merged per second
+# %drqm = percentage of discard requests merged
+# d_await = discard request average wait time
+# dareq-sz = discard request average size
+# f/s = flush requests per second
+# f_await = flush request average wait time
+# aqu-sz = average queue size
+# %util = percentage of CPU utilization
+
+# NETWORK
+ifstat 1 1 >> "$filename"
+# Expected data:
+#      ens5                ens4       
+#KB/s in  KB/s out   KB/s in  KB/s out
+#   0.00      0.00      0.06      0.19
+
+# kb/s = kilobytes per second
+# in = incoming
+# out = outgoing
