a5a04682808f5e53f1f7e0af1d22ed390fb0aa9e
Surxe <119145352+Surxe@users.noreply.github.com>
2025-03-11 23:14:35 -0400

    Fix data dir pathing issue cont.

--- src/get_metrics.sh
+++ src/get_metrics.sh
@@ -5,7 +5,7 @@ sudo apt install sysstat -y
 sudo apt install ifstat -y
 
 # Store the time in a variable
-filename="../data/metrics_$(date +'%Y-%m-%d_%H-%M-%S').txt"
+filename="data/metrics_$(date +'%Y-%m-%d_%H-%M-%S').txt"
 
 # Ensure the directory exists
 mkdir -p data
