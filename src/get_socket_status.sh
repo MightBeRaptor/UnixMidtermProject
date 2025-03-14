56f91bb15036ad345b13c240d08789b870ba2ffc
Surxe <119145352+Surxe@users.noreply.github.com>
2025-03-14 14:51:01 -0400

    Fixed pathing issue with get_socket_status

--- src/get_socket_status.sh
+++ src/get_socket_status.sh
@@ -5,10 +5,10 @@ touch tmp/socket_status.txt
 
 if [ $(netstat -an | grep 127.0.0.1:3300 | grep TCP | grep ESTABLISHED | wc -l) -eq 0 ]; then
     if [ $(netstat -an | grep 127.0.0.1:3300 | grep TCP | grep LISTEN | wc -l) -eq 0 ]; then
-        echo "not listening" >> /tmp/socket_status.txt
+        echo "not listening" >> tmp/socket_status.txt
     else
-        echo "listening for connection" >> /tmp/socket_status.txt
+        echo "listening for connection" >> tmp/socket_status.txt
     fi
 else
-    echo "connected" >> /tmp/socket_status.txt
+    echo "connected" >> tmp/socket_status.txt
 fi
