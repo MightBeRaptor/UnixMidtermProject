95d5e29a3651032c940c68d46198846dc709a178
AgentWebFoot <etg1717@gmail.com>
2025-03-14 19:21:33 -0400

    Update users.sh

--- src/users.sh
+++ src/users.sh
@@ -6,7 +6,7 @@ USER2="bob"
 USER3="charlie"
 
 if ! getent group "$GROUP_NAME" > /dev/null; then
-    groupadd "$GROUP_NAME"
+    sudo groupadd "$GROUP_NAME"
     echo "Group '$GROUP_NAME' created."
 else
     echo "Group '$GROUP_NAME' already exists."
@@ -15,7 +15,7 @@ fi
 create_user() {
     local USERNAME=$1
     if ! id "$USERNAME" &>/dev/null; then
-        useradd -m -G "$GROUP_NAME" "$USERNAME"
+        sudo useradd -m -G "$GROUP_NAME" "$USERNAME"
         echo "User '$USERNAME' created and added to '$GROUP_NAME'."
     else
         echo "User '$USERNAME' already exists."
