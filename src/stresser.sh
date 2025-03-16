e12afc62c5a6ffeae703276c0e222ef4c195f726
AgentWebFoot <etg1717@gmail.com>
2025-03-15 22:50:13 -0400

    Update stresser.sh

--- src/stresser.sh
+++ src/stresser.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-stress-ng --cpu $(nproc) --iomix 4 --hdd 2 --vm-bytes 90% --vm-keep -t $(( (RANDOM % 180) + 30 )) &
+can_run=true
+
+while true; do
+    if $can_run; then
+        can_run=false
+        DURATION=$(( (RANDOM % 180) + 30 ))
+        stress-ng --cpu $(nproc) --iomix 4 --hdd 2 --vm-bytes 90% --vm-keep -t $DURATION &
+
+        (
+            sleep $DURATION
+            can_run=true
+        ) &
+    fi
+    sleep 1
+done
