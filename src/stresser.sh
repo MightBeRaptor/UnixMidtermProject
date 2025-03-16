a61631a7fea3fc79c0ef3d5dcb91be70ac9aa8d7
AgentWebFoot <etg1717@gmail.com>
2025-03-16 16:54:54 -0400

    Update stresser.sh

--- src/stresser.sh
+++ src/stresser.sh
@@ -5,5 +5,5 @@ while true; do
     stress-ng --cpu $(nproc) --iomix 4 --hdd 2 --vm-bytes 90% --vm-keep -t $(( (RANDOM % 180) + 30 ))
 
     # Sleep for a random amount of time
-    sleep($(( (RANDOM % 180) + 30 )))
+    sleep $(( (RANDOM % 180) + 30 ))
 done
